399899cdc10675d41ab8a02992c714a1dd46c303
Gevorg <gevorg@manvelyan-ent.de>
2022-01-14 13:22:53 +0400

    Datatable add show_status default columnRender file config key get by Model className

--- public/js/dashboard/article/index.js
+++ public/js/dashboard/article/index.js
@@ -9,14 +9,6 @@ const options = {
     show: false,
   },
 
-  columnsRender: {
-    show_status: {
-      render(showStatus) {
-        return `<span class="show-status-${showStatus}">${$trans('__dashboard.select.option.show_status_'+showStatus)}</span>`;
-      }
-    }
-  }
-
 };
 // eslint-disable-next-line no-new,no-undef
 new DataTable(options);
