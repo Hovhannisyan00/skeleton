66787964f67bc33e90dfd8c2e1e9db853ece9dc9
gevor <gevor0966@gmail.com>
2023-11-21 16:19:25 +0400

    little changes

--- resources/js/dashboard/dashboard-app.js
+++ resources/js/dashboard/dashboard-app.js
@@ -8,9 +8,9 @@ window.El = {
 require('../bootstrap');
 require('../core/dashboard-init');
 
-require('./plugins/Select2 4.1.0.min');
+require('./plugins/select.min');
 require('./plugins/datatables.net');
-require('./plugins/DataTablesBootstrap');
+require('./plugins/dataTablesBootstrap');
 require('./plugins/jquery.mask.min');
 
 require('jquery-datetimepicker');
