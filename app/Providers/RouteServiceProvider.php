0d412ac6044ab50a417267bd39cb83de1681dbac
gevor <gevor0966@gmail.com>
2024-08-16 10:27:39 +0400

    remove api and channels

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,10 +34,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+        /*    Route::prefix('api')
                 ->middleware('api')
                 ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                ->group(base_path('routes/api.php'));*/
 
             Route::prefix(LaravelLocalization::setLocale())
                 ->middleware([
