fe6567b70bdc470184a74a8944f8f6c8318889dd
gevor <gevor0966@gmail.com>
2024-11-10 17:38:24 +0400

    El notification fix

--- resources/js/common/axios/errorHandler.js
+++ resources/js/common/axios/errorHandler.js
@@ -1,11 +1,9 @@
-import { ElNotification } from 'element-plus/dist/index.full';
-
 // eslint-disable-next-line no-undef
 axios.interceptors.response.use((response) => response, (error) => {
   const resp = error.response;
 
   if (resp.status !== 200 || resp.status !== 201) {
-    ElNotification.error({
+    window.El.Notification.error({
       title: 'Error',
       message: resp.data.message,
       dangerouslyUseHTMLString: true,
